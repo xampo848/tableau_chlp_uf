c9ac240667dfb4b262de4e5f7f1bb5d76e17785e
francisco <francisco.herrera@aimmanager.com>
2019-12-06 15:18:17 -0300

    asdfasdf

---
+++ chilean_uf.js
@@ -0,0 +1,46 @@
+(function () {
+
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) {
+     var cols = [{
+          fecha: "fecha",
+          dataType: tableau.dataTypeEnum.string
+        }, {
+            valor: "valor",
+            dataType: tableau.dataTypeEnum.float
+        }];
+
+        var tableSchema = {
+            id: "uf_clp",
+            alias: "valor uf en clp",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
+        };
+
+    myConnector.getData = function (table, doneCallback) {
+      $.getJSON("https://mindicador.cl/api/uf", function(resp) {
+          var feat = resp.serie;
+              tableData = [];
+          // Iterate over the JSON object
+          tableData.push({
+              "fecha": feat[0].fecha,
+              "valor": feat[0].valor
+              
+          });
+          table.appendRows(tableData);
+          doneCallback();
+      });      
+    };
+    tableau.registerConnector(myConnector);
+
+  $(document).ready(function () {
+      $("#submitButton").click(function () {
+          tableau.connectionName = "chilean uf";
+          tableau.submit();
+      });
+  });
+
+})();
